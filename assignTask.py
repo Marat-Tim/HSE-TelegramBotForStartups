63a01351b63fb69a332731ae6905253caf0b7a59
Derevyagin Alexander <derevyagin102@mail.ru>
2022-09-17 18:22:11 +0300

    Методы изменения принадлежности задачи пользователю improved

--- assignTask.py
+++ assignTask.py
@@ -5,3 +5,9 @@ import DataBase
 def addTaskToUser(id_task: int, current_user: User):
     current_task = DataBase.get_task_by_id(id_task)
     current_task.users.append(User)
+    DataBase.update_task(current_task)
+
+def deleteUserFromTask(id_task: int, current_user: User):
+    current_task = DataBase.get_task_by_id(id_task)
+    current_task.users.remove(User)
+    DataBase.update_task(current_task)
