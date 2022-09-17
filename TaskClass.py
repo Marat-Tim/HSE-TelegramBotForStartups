83b4b81dc84d9505b8ee1b3b8f61c3ea4009c7eb
Derevyagin Alexander <derevyagin102@mail.ru>
2022-09-17 17:27:46 +0300

    Убрал start, добавил users, end теперь типа datetime

--- TaskClass.py
+++ TaskClass.py
@@ -1,7 +1,7 @@
+import datetime
 class TaskClass:
-    def __init__(self, start, end, title, user='None', description=''):
-        self.start = start
+    def __init__(self, title, end=datetime.date(0, 0, 0), users=[], description=''):
         self.end = end
-        self.user = user
+        self.users = users
         self.title = title
         self.description = description
