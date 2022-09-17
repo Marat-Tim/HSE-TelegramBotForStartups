b362a9e756f43a411e1e347747c4dae03e584ea7
Maxim Saprykin <mgsaprykin@edu.hse.ru>
2022-09-17 18:10:22 +0300

    Сделал функцию AddTask

---
+++ AddTask.py
@@ -0,0 +1,18 @@
+from UserClass import  User
+from typing import List
+from TaskClass import TaskClass
+import pickle
+import datetime
+def AddTask(title: str, description: str, deadline: datetime.datetime):
+    try:
+        with open("data/temporary/add_task_last_id.txt", "rb") as f:
+            last_id:int = pickle.load(f)
+    except FileNotFoundError:
+        last_id = 0
+    Task = TaskClass(last_id, title, deadline, [], description)
+    with open(f"data/tasks/{last_id}.task", "wb") as f:
+        pickle.dump(Task, f)
+    last_id += 1
+    with open("data/temporary/add_task_last_id.txt", "wb") as f:
+        pickle.dump(last_id, f)
+
