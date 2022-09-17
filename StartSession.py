cbd9443a7505b38c05406c52aa8facf475d1636c
Marat-Tim <rurikcat@gmail.com>
2022-09-17 21:36:52 +0300

    Переделал startSession

--- StartSession.py
+++ StartSession.py
@@ -1,8 +1,12 @@
+import pickle
+
 from TaskClass import TaskClass
 from UserClass import User
 from Session import Session
 from datetime import datetime
 
-def start_session(task: TaskClass, user: User) -> Session:
+def start_session(task: TaskClass, user: User):
+    now = datetime.now()
     session = Session(user.telegram_id, task.id, datetime.now())
-    return session
+    with open(f"{user.telegram_handle} {now}", 'wb') as f:
+        pickle.dump(session, f)
