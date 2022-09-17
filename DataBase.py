3e0248ed88caf2df6f150d405fa02d0b39d0f900
Marat-Tim <rurikcat@gmail.com>
2022-09-17 20:01:46 +0300

    1

--- DataBase.py
+++ DataBase.py
@@ -11,13 +11,18 @@ class DataBase:
         with open(f"data/{self.path}/{obj.id}", 'w+') as f:
             pickle.dump(obj, f)
 
-    def get_by_id(self, id: int):
+    def get_by_id(self, id):
         with open(f"data/{self.path}/{id}", 'rb') as f:
             return pickle.load(f)
 
     def update(self, obj):
         with open(f"data/{self.path}/{obj.id}", 'wb') as f:
             pickle.dump(obj, f)
+
+    def get_all(self):
+        list = []
+        for f in os.listdir(f"data/{self.path}/"):
+            list.append(self.get_by_id(f))
 
 def get_task_by_id(id: int) -> TaskClass:
     with open(f"data/tasks/{id}.task", 'rb') as f:
