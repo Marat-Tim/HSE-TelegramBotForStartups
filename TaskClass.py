a59efd3808f2512131bc367ba08aa60c7ec3e7fc
Marat-Tim <rurikcat@gmail.com>
2022-09-17 19:10:55 +0300

    сделал преобразование класса в строку

--- TaskClass.py
+++ TaskClass.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 class TaskClass:
     def __init__(self, id: int, title: str, end=datetime.datetime(2000, 1, 1), users=None, description='', is_ready=False):
@@ -8,3 +9,6 @@ class TaskClass:
         self.title = title
         self.description = description
         self.is_ready = is_ready
+
+    def __str__(self):
+        return json.dumps(self)
