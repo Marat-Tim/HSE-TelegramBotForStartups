69f9cd8706358c7464804548f3c6311e0894a301
Derevyagin Alexander <derevyagin102@mail.ru>
2022-09-17 16:42:36 +0300

    Добавление класса TaskClass.py

---
+++ TaskClass.py
@@ -0,0 +1,7 @@
+class TaskClass:
+    def __init__(self, start, end, title, user='None', description=''):
+        self.start = start
+        self.end = end
+        self.user = user
+        self.title = title
+        self.description = description
