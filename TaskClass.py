d4a7b276abd968d8dc0f4d82b8e9c7b2d65083db
Derevyagin Alexander <derevyagin102@mail.ru>
2022-09-17 18:15:26 +0300

    Добавление методов изменения принадлежности задачи пользователю, изменение TaskClass

--- TaskClass.py
+++ TaskClass.py
@@ -1,9 +1,10 @@
 import datetime
 
 class TaskClass:
-    def __init__(self, id: int, title: str, end=datetime.date(0, 0, 0), users=None, description=''):
+    def __init__(self, id: int, title: str, end=datetime.date(0, 0, 0), users=None, description='', is_ready=False):
         self.id = id
         self.end = end
         self.users = users
         self.title = title
         self.description = description
+        self.is_ready = is_ready
---
+++ assignTask.py
@@ -0,0 +1,7 @@
+from UserClass import User
+from TaskClass import TaskClass
+import DataBase
+
+def addTaskToUser(id_task: int, current_user: User):
+    current_task = DataBase.get_task_by_id(id_task)
+    current_task.users.append(User)
