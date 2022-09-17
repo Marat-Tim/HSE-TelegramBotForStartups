77a73793af87cbc8fd2347f56f97a0708049cb9a
Marat-Tim <rurikcat@gmail.com>
2022-09-17 17:57:16 +0300

    Изменил базу данных, добавил фичу all_tasks

--- DataBase.py
+++ DataBase.py
@@ -1,21 +1,8 @@
 import pickle
+from TaskClass import TaskClass
 
-class DataBase:
-    def __init__(self, file_path : str, objects = None):
-        self.file_path = file_path
-        self.data = objects
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(self.data, f)
-
-
-    def add(self, obj):
-        self.data.append(obj)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(self.data, f)
-
-    def get(self):
-        with open(self.file_path, 'rb') as f:
-            self.data = pickle.load(f)
-        return self.data
+def get_task_by_id(id: int) -> TaskClass:
+    with open(f"data/tasks/{id}.task", 'rb') as f:
+        return pickle.load(f)
 
 
---
+++ all_tasks.py
@@ -0,0 +1,11 @@
+import os
+from TaskClass import TaskClass
+import pickle
+
+def get_all_tasks() -> str:
+    text = ""
+    for file in os.listdir("data/tasks"):
+        with open(file, 'rb') as f:
+            text += f"{file}:\n{str(pickle.load(f))}\n"
+    return text
+
