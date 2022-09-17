96edf123587d2e4d697116d803c34ab8a814a8c3
Derevyagin Alexander <derevyagin102@mail.ru>
2022-09-17 17:39:38 +0300

    title: str

--- TaskClass.py
+++ TaskClass.py
@@ -1,7 +1,7 @@
 import datetime
 
 class TaskClass:
-    def __init__(self, title, end=datetime.date(0, 0, 0), users=None, description=''):
+    def __init__(self, title: str, end=datetime.date(0, 0, 0), users=None, description=''):
         self.end = end
         self.users = users
         self.title = title
